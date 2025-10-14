4cb08589322fc551312f0dc06d4e891a42b8393e
DoyeonKing <23301153@bjtu.edu.cn>
2025-10-14 18:38:51 +0800

    表结构修改
    在患者的表结构处新增了老师/学生的字段用于区分

--- vue/src/views/404.vue
+++ vue/src/views/404.vue
@@ -2,9 +2,9 @@
   <div>
     <img src="@/assets/404.png" alt="">
 
-    <div>
-      <el-button type="info" style="width: 50px;height: 20px" @click="router.push('/loginform')"></el-button>
-    </div>
+<!--    <div>-->
+<!--      <el-button type="info" style="width: 50px;height: 20px" @click="router.push('/loginform')"></el-button>-->
+<!--    </div>-->
 
 
 
